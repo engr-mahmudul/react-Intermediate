cbcbb74e83ceefc1db2639ba90140ae1773431e5
Mahmudul Hasan <17101120@uap-bd.edu>
2021-09-28 13:54:20 +0600

    bootstrap intsall by command

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from './components/Card/Card';
+import { Button } from 'react-bootstrap';
 
 function App() {
   const items = [
@@ -11,10 +13,19 @@ function App() {
     { name: 'pen5', price: 180, img: 'https://5.imimg.com/data5/SELLER/Default/2020/10/PX/KF/AW/20193325/ink-pen-500x500.jpg' },
   ]
   return (
-    <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4">
+    <div>
+
+      
+      <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4">
       {
         items.map(item =><Card item={item}></Card>)
       }
+     
+    </div>
+    <Button variant="warning">This is a button</Button>
+    <div>
+
+    </div>
     </div>
   );
 }
